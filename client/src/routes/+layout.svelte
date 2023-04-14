634c22889e32ba2d67a4dc8a368bb9349e94b09c
Christian Payne <christianpayne522@gmail.com>
2023-04-13 19:17:10 -0700

    Svelte Kit client structured out.

---
+++ client/src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+  import "../app.css";
+</script>
+
+<nav class="w-full bg-teal-200 flex p-4 items-center justify-between">
+  <h1 class="text-3xl">World Happiness</h1>
+  <div class="text-xl space-x-4">
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+  </div>
+</nav>
+
+<div class="p-4">
+  <slot />
+</div>
