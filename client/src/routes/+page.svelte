87910773cd67260261614f9428690f8ca340cd65
Christian Payne <christianpayne522@gmail.com>
2023-04-18 23:02:27 -0700

    Got plotly working.

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,20 +1,22 @@
 <script lang="ts">
   import { onMount } from 'svelte'; 
-  import Plotly from "plotly.js-dist-min";
+  // import Plotly from "plotly.js-dist-min";
   import Navigation from "./navigation.svelte";
   const buttonClasses = 'block md:inline-block p-4 md:w-1/3 border border-accent border-b-4 active:border-b-[1px] active:translate-y-1 rounded-md'
   
 
-  // export let data = [{
-  //   x: ['giraffes', 'orangutans', 'monkeys'],
-  //   y: [20, 14, 23],
-  //   type: 'bar'
-  // }];
-  // let plotDiv: Element;
+  let data = [{
+    x: ['giraffes', 'orangutans', 'monkeys'],
+    y: [20, 14, 23],
+    type: 'bar'
+  }];
+  let plotDiv: Element;
 
-  // onMount(() => {
-  //   new Plotly.newPlot(plotDiv, data, {}, {showSendToCloud:true}); 
-  // });
+  onMount(async () => {
+    const Plotly = await import('plotly.js-dist-min')
+    // @ts-ignore
+    new Plotly.newPlot(plotDiv, data, {}, {showSendToCloud:true});
+  });
 </script>
 
 <div class="flex text-lg md:text-3xl p-4 md:h-screen md:items-center md:justify-center flex-col justify-around text-dark-gray">
@@ -28,7 +30,7 @@
     <!-- <h2 class="mx-auto bg-accent w-1/2 h-32">
       -Graph-
     </h2> -->
-    <!-- <div bind:this={plotDiv}>Plotly chart will be drawn inside this DIV</div> -->
+    <div bind:this={plotDiv}><!-- Plotly chart will be drawn inside this DIV --></div>
     <div class="block md:flex justify-around space-y-4 md:space-y-0 md:space-x-4">
       <a href="data" class="{buttonClasses}">
         Explore Data
