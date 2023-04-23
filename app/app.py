fad913f831882eab658eb59ddd7e72a61d556b8c
ennegineer <enne.face@gmail.com>
2023-04-22 20:05:06 -0700

    set up Chalice and DynamoDB

--- app/app.py
+++ app/app.py
@@ -1,11 +1,67 @@
+import os
+
+import boto3
 from chalice import Chalice
+from chalicelib import db
+
 
 app = Chalice(app_name='chaliceapp')
+app.debug = True
+_DB = None
+_USER_DB = None
+_AUTH_KEY = None
+_SSM_AUTH_KEY_NAME = '/todo-sample-app/auth-key'
 
 
-@app.route('/')
-def index():
-    return {'hello': 'world'}
+# @app.route('/login', methods=['POST'])
+# def login():
+#     body = app.current_request.json_body
+#     record = get_users_db().get_item(
+#         Key={'username': body['username']})['Item']
+#     jwt_token = auth.get_jwt_token(
+#         body['username'], body['password'], record, get_auth_key())
+#     return {'token': jwt_token}
+
+
+# def get_users_db():
+#     global _USER_DB
+#     if _USER_DB is None:
+#         _USER_DB = boto3.resource('dynamodb').Table(
+#             os.environ['USERS_TABLE_NAME'])
+#     return _USER_DB
+
+
+def get_app_db():
+    global _DB
+    if _DB is None:
+        _DB = db.DynamoDB(
+            boto3.resource('dynamodb').Table(
+                os.environ['worldhappiness'])
+        )
+    return _DB
+
+
+@app.route('/', methods=['GET'])
+def all_items():
+    return get_app_db().list_all_items()
+
+@app.route('/happiness', methods=['GET'])
+# list the countries and years available to query
+def list_countries():
+    return get_app_db().list_items()
+
+
+@app.route('/happiness/{country}', methods=['GET'])
+# return all data available per country
+def all_data_per_country(country):
+    return get_app_db().list_items(country)
+
+@app.route('/happiness/{country}/{year}', methods=['GET'])
+# return the data for a specific country and year combination
+def country_by_year(country, year):
+    return get_app_db().get_item(country, year)
+
+
 
 
 # The view function above will return {"hello": "world"}
---
+++ app/chalicelib/db.py
@@ -0,0 +1,129 @@
+from uuid import uuid4
+
+from boto3.dynamodb.conditions import Key
+
+
+DEFAULT_USERNAME = 'default'
+
+
+class happyDB(object):
+    def list_items(self):
+        pass
+
+    def add_item(self, description, metadata=None):
+        pass
+
+    def get_item(self, uid):
+        pass
+
+    def delete_item(self, uid):
+        pass
+
+    def update_item(self, uid, description=None, state=None,
+                    metadata=None):
+        pass
+
+
+class InMemoryHappyDB(happyDB):
+    def __init__(self, state=None):
+        if state is None:
+            state = {}
+        self._state = state
+
+    def list_all_items(self):
+        all_items = []
+        for country in self._state:
+            all_items.extend(self.list_items(country))
+        return all_items
+
+    def list_items(self, country):
+        return list(self._state.get(country, {}).values())
+
+    def add_item(self, description, metadata=None, username=DEFAULT_USERNAME):
+        if username not in self._state:
+            self._state[username] = {}
+        uid = str(uuid4())
+        self._state[username][uid] = {
+            'uid': uid,
+            'description': description,
+            'state': 'unstarted',
+            'metadata': metadata if metadata is not None else {},
+            'username': username
+        }
+        return uid
+
+    def get_item(self, country, year):
+        return self._state[country][year]
+
+    def delete_item(self, uid, username=DEFAULT_USERNAME):
+        del self._state[username][uid]
+
+    def update_item(self, uid, description=None, state=None,
+                    metadata=None, username=DEFAULT_USERNAME):
+        item = self._state[username][uid]
+        if description is not None:
+            item['description'] = description
+        if state is not None:
+            item['state'] = state
+        if metadata is not None:
+            item['metadata'] = metadata
+
+
+class DynamoDB(happyDB):
+    def __init__(self, table_resource):
+        self._table = table_resource
+
+    def list_all_items(self):
+        response = self._table.scan()
+        return response['Items']
+
+    def list_items(self, country):
+        response = self._table.query(
+            KeyConditionExpression=Key('country').eq(country)
+        )
+        return response['Items']
+
+    def add_item(self, description, metadata=None, username=DEFAULT_USERNAME):
+        uid = str(uuid4())
+        self._table.put_item(
+            Item={
+                'username': username,
+                'uid': uid,
+                'description': description,
+                'state': 'unstarted',
+                'metadata': metadata if metadata is not None else {},
+            }
+        )
+        return uid
+
+    def get_item(self, country, year):
+        try:
+            response = self._table.get_item(
+            Key={
+                'country': country,
+                'year': int(year),
+            },
+            )
+            return response['Item']
+        except:
+            return 'The country or year you searched for could not be found. Note: the data spans years 2005 to 2022.'
+
+    def delete_item(self, uid, username=DEFAULT_USERNAME):
+        self._table.delete_item(
+            Key={
+                'username': username,
+                'uid': uid,
+            }
+        )
+
+    def update_item(self, uid, description=None, state=None,
+                    metadata=None, username=DEFAULT_USERNAME):
+        # We could also use update_item() with an UpdateExpression.
+        item = self.get_item(uid, username)
+        if description is not None:
+            item['description'] = description
+        if state is not None:
+            item['state'] = state
+        if metadata is not None:
+            item['metadata'] = metadata
+        self._table.put_item(Item=item)
