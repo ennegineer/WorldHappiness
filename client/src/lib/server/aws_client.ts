aa7c72486b4c7e13002f49a07049b96135fbd098
Christian Payne <christianpayne522@gmail.com>
2023-06-04 18:33:14 -0700

    Working code for pulling data from our API. Updated Readme. Env setup and documenation as well.

--- client/src/lib/server/aws_client.ts
+++ client/src/lib/server/aws_client.ts
@@ -1,1 +1,14 @@
-import { PUBLIC_TEST } from '$env/static/public';
+import { BASE_URL } from '$env/static/private';
+
+async function getData(slug: string) {
+  try {
+    let response = await fetch(BASE_URL + slug)
+    return await response.json()
+  } catch (error) {
+    throw new Error("Failed to fetch data from database api.")
+  }
+}
+
+export async function getCountryList(): Promise<string[]> {
+  return getData("happiness/country")
+}
--- client/src/routes/explore/+page.svelte
+++ client/src/routes/explore/+page.svelte
@@ -1,12 +1,26 @@
 <script lang="ts">
+	import type { PageData } from './$types';
 	import Navigation from '../navigation.svelte';
 	import RouteButtons from '../routeButtons.svelte';
 
 	let checkbox = false;
+
+	export let data: PageData;
+	let countryList = data.countryList
+	let selectedCountry: string = countryList[0]
 </script>
 
 <Navigation />
 <div class="p-4">
 	<h1>Explore the Data</h1>
+
+	<select bind:value={selectedCountry} class="p-2 m-2 border-2 border-coral rounded-md">
+		{#each countryList as country}
+			<option value={country}>
+				{country}
+			</option>
+		{/each}
+	</select>
 	<RouteButtons buttonsToInclude={['about', 'meet']} />
 </div>
+
