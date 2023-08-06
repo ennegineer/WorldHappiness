2999e6d6be73ec49b68898ed33aaa2638a939bca
Christian Payne <christianpayne522@gmail.com>
2023-08-06 01:34:06 -0700

    Fixed types in YoY chart config.

--- client/src/routes/explore/charts/yearOverYearHappinessData.chart.ts
+++ client/src/routes/explore/charts/yearOverYearHappinessData.chart.ts
@@ -3,17 +3,17 @@ import type { ChartConfiguration } from "chart.js/auto";
 type ChartInput = {
   countryName: string
   data: {
-    year: string
-    confidence_in_national_government: string
-    freedom_to_make_life_choices: string
-    log_gdp_per_capita: string
-    healthy_life_expectancy_at_birth: string
-    positive_affect: string
-    life_ladder: string
-    generosity: string
-    perceptions_of_corruption: string
-    negative_affect: string
-    social_support: string
+    year: number
+    confidence_in_national_government: number
+    freedom_to_make_life_choices: number
+    log_gdp_per_capita: number
+    healthy_life_expectancy_at_birth: number
+    positive_affect: number
+    life_ladder: number
+    generosity: number
+    perceptions_of_corruption: number
+    negative_affect: number
+    social_support: number
   }[]
 }
 
@@ -21,39 +21,39 @@ export function getChartConfiguration (input: ChartInput): ChartConfiguration {
   return {
     type: 'line',
     data: {
-      labels: input.data.map((row: any) => row.year),
+      labels: input.data.map(row => row.year),
       datasets: [
         {
           label: 'Confidence in National Government',
-          data: input.data.map((row: any) => (row.confidence_in_national_government * 100).toFixed(2))
+          data: input.data.map(row => Math.round(row.confidence_in_national_government * 100))
         },
         {
           label: 'Freedom to Make Life Choices',
-          data: input.data.map((row: any) => (row.freedom_to_make_life_choices * 100).toFixed(2))
+          data: input.data.map(row => Math.round(row.freedom_to_make_life_choices * 100))
         },
         {
           label: 'GDP Per Capita (nat. log)',
-          data: input.data.map((row: any) => row.log_gdp_per_capita)
+          data: input.data.map(row => Math.round(row.log_gdp_per_capita))
         },
         {
           label: 'Positive Affect',
-          data: input.data.map((row: any) => (row.positive_affect * 100).toFixed(2))
+          data: input.data.map(row => Math.round(row.positive_affect * 100))
         },
         {
           label: 'Negative Affect',
-          data: input.data.map((row: any) => (row.negative_affect * 100).toFixed(2))
+          data: input.data.map(row => Math.round(row.negative_affect * 100))
         },
         {
           label: 'Generosity',
-          data: input.data.map((row: any) => (row.generosity * 100).toFixed(2))
+          data: input.data.map(row => Math.round(row.generosity * 100))
         },
         {
           label: 'Perceptions Of Corruption',
-          data: input.data.map((row: any) => (row.perceptions_of_corruption * 100).toFixed(2))
+          data: input.data.map(row => Math.round(row.perceptions_of_corruption * 100))
         },
         {
           label: 'Social Support',
-          data: input.data.map((row: any) => (row.social_support * 100).toFixed(2))
+          data: input.data.map(row => Math.round(row.social_support * 100))
         }
       ]
     },
